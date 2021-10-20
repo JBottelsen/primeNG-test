fabc0710b57152513aff6a82d5b3a8643713fcd5
Jared <jared@bulbapp.com>
2021-10-20 16:52:20 -0600

    Created a component to test focus out directive with lose info modal type functionality

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { ClickOutsideModule } from 'ng-click-outside'
 
 import { ButtonModule } from 'primeng/button';
 import { CardModule } from 'primeng/card';
 import { MultiSelectModule } from 'primeng/multiselect';
 import { SelectButtonModule } from 'primeng/selectbutton';
 import { OverlayPanelModule } from 'primeng/overlaypanel';
+import { AccordionModule } from 'primeng/accordion';
+import { InputTextModule } from 'primeng/inputtext';
+import { ConfirmDialogModule } from 'primeng/confirmdialog';
+import { Messages, MessagesModule } from 'primeng/messages';
 
 import { HttpService } from './services/http.service';
 import { AppComponent } from './app.component';
@@ -16,6 +21,7 @@ import { AppComponent } from './app.component';
 import { AsyncDropdownComponent } from './components/async-dropdown/async-dropdown.component';
 import { ToggleFormComponent } from './components/toggle-form/toggle-form.component';
 import { SharePanelComponent } from './components/share-panel/share-panel.component';
+import { FocusOutComponent } from './components/accordion-test/focus-out/focus-out.component';
 
 @NgModule({
   declarations: [
@@ -23,19 +29,25 @@ import { SharePanelComponent } from './components/share-panel/share-panel.compon
     AsyncDropdownComponent,
     ToggleFormComponent,
     SharePanelComponent,
+    FocusOutComponent,
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     FormsModule,
     ReactiveFormsModule,
+    ClickOutsideModule,
 
     // PrimeNG
+    AccordionModule,
     ButtonModule,
     CardModule,
     MultiSelectModule,
     OverlayPanelModule,
     SelectButtonModule,
+    InputTextModule,
+    ConfirmDialogModule,
+    MessagesModule
   ],
   providers: [
     HttpService,
